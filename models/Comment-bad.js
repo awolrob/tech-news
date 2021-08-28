6ec8e8acd1f9942f93e42e695caa269b18efb067
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-08-28 19:51:28 -0400

    error

--- models/Comment-bad.js
+++ models/Comment-bad.js
@@ -1,7 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+class Comment extends Model { }
+//
 
 Comment.init(
   {
