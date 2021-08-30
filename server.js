6086b6877339e170badb584ad5c9846a29018f46
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-08-29 22:48:22 -0400

    heroku

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 //initalize database; then seed 
-// sequelize.sync({ force: true }).then(() => {
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
+// sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening  ${PORT}`));
 });
